2613fe7a30ab585539719256e93b2936243672d4
iddimsangi <adammsangi@gmail.com>
2021-07-23 09:39:14 +0300

    render contact card from contact card component

--- src/Components/ContactCard.js
+++ src/Components/ContactCard.js
@@ -1,1 +1,21 @@
-rc
+import React from 'react'
+
+function ContactCard(props) {
+    const {id, name, email} = props.contacts
+    return (
+        <div>
+            <div style={{marginTop:'20px', border:'1px solid #ccc', padding:'10px'}} class="ui relaxed divided list">
+                <div class="item">
+                <i class="large github middle aligned icon"></i>
+                <div class="content">
+                <a class="header">{name}</a>
+                <div class="description">{email}</div>
+                </div>
+                <i class="trash alternate outline icon"></i>
+                </div>    
+             </div>
+        </div>
+    )
+}
+
+export default ContactCard
--- src/Components/ContactsList.js
+++ src/Components/ContactsList.js
@@ -1,19 +1,12 @@
 import React from 'react'
-
+import ContactCard from './ContactCard'
 function ContactsList(props) {
-    const contactsRenders = props.contactLst
+    const contactsRenders = props.contactLst.map(contact =>{
+        return <ContactCard contacts={contact} />
+    })
     return (
         <div >
-            <div style={{marginTop:'20px', border:'1px solid #ccc', padding:'10px'}} class="ui relaxed divided list">
-                <div class="item">
-                <i class="large github middle aligned icon"></i>
-                <div class="content">
-                    <a class="header">Semantic-Org/Semantic-UI</a>
-                    <div class="description">Updated 10 mins ago</div>
-                </div>
-                <i class="trash alternate outline icon"></i>
-          </div>    
-           </div>
+            {contactsRenders}
         </div>
        
     )
