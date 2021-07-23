80606472508dd487cd3ec83d8b137206f8a4793a
iddimsangi <adammsangi@gmail.com>
2021-07-23 11:00:14 +0300

    add state in addcard component

--- src/Components/AddContacts.js
+++ src/Components/AddContacts.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react'
 import { Button, Checkbox, Form } from 'semantic-ui-react'
 export default class AddContacts extends Component {
+    state = {
+        name:"",
+        email:""
+    }
+    addContact = (e) =>{
+        e.preventDefault();
+        if(this.state.name === "" || this.state.email === ""){
+            alert('All fields are mandatory');
+            return
+        }
+        console.log(this.state);
+        this.setState({
+            name:"",
+            email:""
+        })
+    } 
     render() {
         return (
             <div style={{marginTop:'30px'}}>
                 <h2>Add Contact</h2>
-            <Form>
+            <Form onSubmit={this.addContact}>
                 <Form.Field>
                 <label>First Name</label>
-                <input placeholder='First Name' />
+                <input placeholder='First Name' 
+                onChange={(e) =>{this.setState({name:e.target.value})}}
+                value={this.state.name} />
                 </Form.Field>
                 <Form.Field>
                 <label>Email</label>
-                <input placeholder='Email' />
+                <input placeholder='Email' 
+                onChange={(e) =>{this.setState({email:e.target.value})}}
+                value={this.state.email} />
                 </Form.Field>
                 <Form.Field>
                 <Checkbox label='I agree to the Terms and Conditions' />
--- src/Components/App.js
+++ src/Components/App.js
@@ -1,27 +1,28 @@
-
+import React,{useState} from 'react'
 import './App.css';
 import Header from '../Components/Header'
 import AddContacts from '../Components/AddContacts'
 import ContactsList from '../Components/ContactsList'
 
 function App() {
-  const contacts = [
-    {
-      id:'1',
-      name:'iddiMsangi',
-      email:'imsangi@icloud.com'
-    },
-    {
-      id:'2',
-      name:'Ally kilu',
-      email:'kiluda@amana.com'
-    },
-    {
-      id:'3',
-      name:'harith kilu',
-      email:'Harith@icloud.com'
-    }
-  ]
+const [contacts, setContacts] = useState([]);
+  // const contacts = [
+  //   {
+  //     id:'1',
+  //     name:'iddiMsangi',
+  //     email:'imsangi@icloud.com'
+  //   },
+  //   {
+  //     id:'2',
+  //     name:'Ally kilu',
+  //     email:'kiluda@amana.com'
+  //   },
+  //   {
+  //     id:'3',
+  //     name:'harith kilu',
+  //     email:'Harith@icloud.com'
+  //   }
+  // ]
   return (
     <div className="ui container">
         <Header/>
